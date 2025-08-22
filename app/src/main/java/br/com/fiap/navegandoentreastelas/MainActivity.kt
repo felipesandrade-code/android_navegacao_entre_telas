390fe67f5cccda09542e6296313c82edcb60facf
Felipe Andrade <feandrade.work@gmail.com>
2025-08-22 16:27:45 -0300

    Commit para a inserção de efeitos de transição entre as telas, porém com a biblioteca antiga que será descontinuada do Kotlin, futuramente fazer a manutenção para realizar o efeito da maneira atual.

--- app/src/main/java/br/com/fiap/navegandoentreastelas/MainActivity.kt
+++ app/src/main/java/br/com/fiap/navegandoentreastelas/MainActivity.kt
@@ -4,23 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import androidx.navigation.navArgument
 import br.com.fiap.navegandoentreastelas.screens.MenuScreen
 import br.com.fiap.navegandoentreastelas.screens.PedidosScreen
 import br.com.fiap.navegandoentreastelas.screens.PerfilScreen
 import br.com.fiap.navegandoentreastelas.ui.theme.NavegandoEntreAsTelasTheme
 import br.com.fiap.navegandoentreastelas.screens.LoginScreen
+import androidx.compose.animation.AnimatedContentTransitionScope
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,8 +35,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    val navController = rememberNavController()
-                    NavHost(navController = navController, startDestination = "login"){
+                    val navController = rememberAnimatedNavController()
+                    AnimatedNavHost(
+                        navController = navController,
+                        startDestination = "login",
+                        exitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentTransitionScope.SlideDirection.End,
+                                animationSpec = tween (1000)
+                            )
+                        },
+                        enterTransition = {
+                            slideIntoContainer(
+                                AnimatedContentTransitionScope.SlideDirection.Start,
+                                animationSpec = tween(1000)
+                            )
+                        }
+                    ){
                         composable(route = "login"){
                             LoginScreen(navController)
                         }
